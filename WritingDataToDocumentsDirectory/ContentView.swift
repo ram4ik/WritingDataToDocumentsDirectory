633a0c523d510b346f70feff66957216b715a03b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-30 08:53:08 +0200

    init

--- WritingDataToDocumentsDirectory/ContentView.swift
+++ WritingDataToDocumentsDirectory/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    
     var body: some View {
         Text("Hello, World!")
+            .onTapGesture {
+                let str = "Test Massage"
+                let url = self.getDocumentsDirectory()
+                    .appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+        }
     }
 }
 
